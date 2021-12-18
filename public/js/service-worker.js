850f2454c244c0c9ee08b10934974d7b5420eb24
TylerMair <tyleramair@gmail.com>
2021-12-18 09:39:23 -0700

    corrected file names to cache

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
   '/js/index.js',
   '/manifest.json',
   '/js/idb.js',
-  '/css/style.css',
+  '/css/styles.css',
   '/icons/icon-72x72.png',
   '/icons/icon-96x96.png',
   '/icons/icon-128x128.png',
