64b0039a22787eae42ccc41e9b6ffb298999d401
TylerMair <tyleramair@gmail.com>
2021-12-18 09:30:36 -0700

    service worker script corrected

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -22,7 +22,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!');
+      console.log('Your files were pre-cached successfully!',cache);
       return cache.addAll(FILES_TO_CACHE);
     })
   );
