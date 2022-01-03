a33a5210c70a7f15cab63fb9133a6e8ff2679a6c
TylerMair <tyleramair@gmail.com>
2022-01-03 14:14:26 -0700

    deploy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/PWA-19";
+
 
 const app = express();
 
@@ -16,9 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
