16de8e6c8924d7f5ccf3212a167af4ac3ad6dd4a
TylerMair <tyleramair@gmail.com>
2022-01-03 14:04:25 -0700

    deploy

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useFindAndModify: false,
 });
 
 // routes
